5c6d48083a8d5e3652ed6db838fd2badb66bc47b
Taiga <matsumoto_taiga@outlook.jp>
2020-04-03 12:20:31 +1000

    UPDATE

--- 136.py
+++ 136.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 
 
 class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
+    def singleNumber(self, nums) -> int:
         d = defaultdict(int)
         for i in nums:
             d[i] += 1
